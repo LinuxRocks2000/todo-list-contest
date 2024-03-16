d6a3e114a1fc61a5a477a0eb94e7785d6d16e2b2
Lazy Hippopotamus <lazy@hippo.com>
2024-03-16 15:01:25 -0400

    how ya like muh now? gonna have to run this through a minifier lol

--- dom.js
+++ dom.js
@@ -1,1 +1,87 @@
+var $_topid = Math.round(Math.random() * 10000);
 function $(id, scope = document) { if (id == "body") { return document.body; } var t = scope.getElementById(id); if (t) { return t; } t = scope.getElementsByClassName(id); if (t.length > 0) { return t; } t = scope.getElementsByTagName(id); if (t.length > 0) { return t; } return scope.querySelector(id); }
+function $_(type, parameters = {}, ...args) {
+    var l = document.createElement(type);
+    l.$a = function () {
+        $a(this, ...arguments);
+    };
+    l._interactions = [];
+    l.$i = function (cbk) {
+        l.addEventListener("click", cbk);
+    };
+    Object.keys(parameters).forEach(key => {
+        if (key == "class") {
+            if (Array.isArray(parameters[key])) {
+                parameters[key].forEach(cl => {
+                    l.classList.add(cl);
+                });
+            }
+            else {
+                l.classList.add(parameters[key]);
+            }
+        }
+        else if (key == "for") {
+            l["htmlFor"] = parameters[key];
+        }
+        else {
+            l[key] = parameters[key];
+        }
+    });
+    args.forEach(arg => {
+        if (typeof arg == "string") {
+            l.appendChild(document.createTextNode(arg));
+        }
+        else {
+            l.appendChild(arg);
+        }
+    });
+    return l;
+}
+function $e(el, cbk) {
+    el.contentEditable = true;
+    var target = { data: "" };
+    const p = {
+        get(target, prop, receiver) {
+            return Reflect.get(...arguments);
+        },
+        set(target, prop, value, receiver) {
+            var reflected = Reflect.set(...arguments);
+            if (prop == "data") {
+                if (el.innerText != value) {
+                    el.innerText = value;
+                }
+                if (cbk) {
+                    cbk(receiver, el);
+                }
+            }
+            return reflected;
+        }
+    }
+    var ret = new Proxy(target, p);
+    el.addEventListener("input", () => {
+        ret.data = el.innerText;
+    });
+    ret.data = el.innerText;
+    return ret;
+}
+function $i(el, cbk) {
+    el.$i(cbk);
+    return el;
+}
+function $k(el, cbk) {
+    el._interactions.push(cbk);
+    return el;
+}
+function $a(parent, ...children) { children.forEach(child => parent.appendChild(child)) }
+function $id(el) { el.id = "e41C" + $_topid++; return el; }
+function $t(el, ind=1) { el.tabIndex = ind; return el; }
+
+window.addEventListener("keyup", (evt) => {
+    if (evt.key == "Enter") { // enter-key interaction
+        if (document.activeElement._interactions) {
+            document.activeElement._interactions.forEach(interaction => {
+                interaction.call(document.activeElement);
+            });
+        }
+    }
+});
--- main.js
+++ main.js
@@ -2,362 +2,108 @@ if (!localStorage.list) { // set a default if necessary
     localStorage.list = `[]`;
 }
 
-// we use a Proxy to manipulate localStorage and the DOM while outwardly only doing simple manipulations on an array
-// this is not necessarily the most efficient way to do it, but saves a LOT of syntactical hell
 
-var _target = undefined;
-var list = undefined;
-var renderedOutCounter = 0;
-var hovered = undefined;
-var hoverParent = undefined;
-var percModeFrac = false;
-function delThing() {
-    hoverParent.remove(hovered);
-    completenessUpdate();
-}
-
-const listProxyHandler = {
-    get(target, property, me) {
-        if (property == "identify") {
-            return true;
-        }
-        if (property == "remove") {
-            return (item) => {
-                item.el.parentNode.removeChild(item.el);
-                for (var i = 0; i < target.length; i++) {
-                    if (target[i] == item) {
-                        target.splice(i, 1);
-                        break;
-                    }
-                }
-                commit();
-                if (hovered == item) {
-                    hovered = undefined;
-                    hoverParent = undefined;
-                    $("utilbar").style.display = "none";
-                }
-            };
-        }
-        if (property == "push") {
-            return (thing) => {
-                if (Array.isArray(thing)) {
-                    thing = new Proxy(thing, listProxyHandler);
-                }
-                thing.el = renderElement(thing, me);
-                target.push(thing);
-                (target.el.tagName == "UL" ? target.el : target.el.querySelector("ul")).appendChild(thing.el);
-                commit();
-            };
-        }
-        if (parseInt(property) != undefined) {
-            if (!Array.isArray(target[property])) {
-                return target[property];
-            }
-            if (!target[property].identify) {
-                target[property] = new Proxy(target[property], listProxyHandler);
-            }
-            return target[property];
-        }
-        return target[property]; // I *could* use a Reflect for this. Couldn't I. Hat hat hat.
-    },
-    set(target, property, value) {
-        if (typeof property == "number") {
-            target[property] = value;
-            commit();
-        }
-        if (property == "el") {
-            target.el = value;
-        }
+class Task { // a Task that contains a Weight, a State, and 0 or more subtasks
+    // a Task's total weight is equal to weight + the weight of every subtask
+    // a task is complete if it has the complete flag and every subtask has the complete flag
+    constructor(nameText, data, weight) {
+        this.parent = undefined;
+        this.name = name;
+        this.data = data;
+        this.weight = weight;
+        this.isComplete = false;
+        this.subtasks = [];
+        var me = this;
+        var name = $k($t($_("div", { class: "name" }, nameText)), () => {
+            me.name.data = me.name.data.replace("\n", "");
+        });
+        this.name = $e(name);
+        this.element = $_("div", { class: "task" }, name);
+        this.stub = true;
     }
-};
 
-function load() {
-    _target = JSON.parse(localStorage.list);
-    list = new Proxy(_target, listProxyHandler);
-}
-
-function commit() {
-    localStorage.list = JSON.stringify(_target);
-}
-
-function addThing(point) {
-    if (point == undefined) {
-        point = hovered;
-    }
-    point.push({
-        name: "Untitled",
-        content: "",
-        weight: 1,
-        complete: false
-    });
-    completenessUpdate();
-}
-
-function addDir(point) {
-    if (point == undefined) {
-        point = hovered;
-    }
-    point.push([]);
-}
-
-function goToEditor(thing) {
-    console.log(thing);
-    $("content").innerHTML = thing.content;
-    $("content").oninput = () => {
-        thing.content = $("content").innerHTML;
-        commit();
-    };
-    $("weight").value = thing.weight;
-    $("weight").oninput = () => {
-        var v = $("weight").value;
-        if (v == "" || v < 1) { // catch the failure cases with invalid inputs
-            thing.weight = 1;
-        }
-        else {
-            thing.weight = v - 0;
-        }
-        $("weight").value = thing.weight;
-        commit();
-        completenessUpdate();
-    };
-    $("checkboxy").checked = thing.complete;
-    $("checkboxy").onclick = () => {
-        thing.complete = $("checkboxy").checked;
-        commit();
-        completenessUpdate();
-    };
-    $("completion").style.display = "";
-}
-
-
-function renderElement(at, parent) { // do the INITIAL render of the tree (calling render over and over again is a slow and terrible idea)
-    let item = document.createElement("li");
-    at.el = item;
-    if (Array.isArray(at)) {
-        item.classList.add("nest");
-        var input = document.createElement("input");
-        input.type = "checkbox";
-        input.id = "nestbox" + renderedOutCounter;
-        input.classList.add("nest-box");
-        var label = document.createElement("label");
-        label.tabIndex = 0;
-        label.setAttribute("for", "nestbox" + renderedOutCounter);
-        var nest = document.createElement("ul");
-        var perc = document.createElement("span");
-        perc.classList.add("percentage");
-        perc.onclick = (evt) => {
-            percModeFrac = !percModeFrac;
-            completenessUpdate();
-            evt.stopPropagation();
+    complete() { 
+        if (!this.isComplete) {
             return false;
-        };
-        item.appendChild(input);
-        item.appendChild(label);
-        item.appendChild(nest);
-        label.appendChild(perc);
-        for (var i = 0; i < at.length; i++) {
-            nest.appendChild(renderElement(at[i], at));
         }
-        item.onmouseover = (evt) => {
-            if (!hovered || hovered != at) {
-                var utilbar = $("utilbar");
-                utilbar.style.display = "";
-                utilbar.classList.add("directoryMode");
-                utilbar.classList.remove("itemMode");
-                utilbar.style.left = item.getBoundingClientRect().right + "px";
-                utilbar.style.top = item.getBoundingClientRect().top + "px";
-                hovered = at;
-                hoverParent = parent;
+        for (var i = 0; i < this.subtasks.length; i++) {
+            if (!this.subtasks[i].complete()) {
+                return false;
             }
-            if (evt) { evt.stopPropagation(); }
-        };
+        }
+        return true;
     }
-    else {
-        item.innerText = at.name;
-        item.onclick = () => {
-            goToEditor(at);
-        };
-        item.contentEditable = true;
-        item.oninput = () => {
-            at.name = item.innerText.replace("\n", "").trim();
-            if (item.innerText != at.name) {
-                item.innerText = at.name;
-            }
-            commit();
-        };
-        item.onmouseover = (evt) => {
-            if (!hovered || hovered != at) {
-                var utilbar = $("utilbar");
-                utilbar.style.display = "";
-                utilbar.classList.add("itemMode");
-                utilbar.classList.remove("directoryMode");
-                utilbar.style.left = item.getBoundingClientRect().right + "px";
-                utilbar.style.top = item.getBoundingClientRect().top + "px";
-                hovered = at;
-                hoverParent = parent;
-            }
-            if (evt) { evt.stopPropagation(); }
-        };
-    }
-    renderedOutCounter++;
-    return item;
-}
 
-function completenessUpdate(thing) { // render complete-ness values for every containing element
-    // returns a [complete, total] array for utility
-    var ret = [0, 0];
-    if (thing == undefined) {
-        thing = list;
+    getContainer() {
+        return this.element;
     }
-    for (var i = 0; i < thing.length; i++) {
-        if (Array.isArray(thing[i])) {
-            var d = completenessUpdate(thing[i]);
-            ret[0] += d[0];
-            ret[1] += d[1];
-        }
-        else {
-            ret[0] += thing[i].complete ? thing[i].weight : 0;
-            ret[1] += thing[i].weight;
-        }
-    }
-    thing.el.style.setProperty("--completeness", (ret[0] / ret[1] * 100) + "%");
-    var writeOutEl = undefined;
-    if (thing != list) {
-        writeOutEl = thing.el.querySelector("label > span.percentage");
-    }
-    else {
-        writeOutEl = $("globalCompletion");
-    }
-    writeOutEl.innerText = ret[1] == 0 ? 0 : (percModeFrac ? (ret[0] + "/" + ret[1]) : (Math.round(ret[0] / ret[1] * 100) + "%"));
-    return ret;
-}
 
-load();
-var r = $("navInside");
-r.innerHTML = ""; // "quick and dirty" way to clear the todo list tree
-var todoListRoot = document.createElement("ul");
-list.el = todoListRoot;
-for (var i = 0; i < list.length; i++) {
-    todoListRoot.appendChild(renderElement(list[i], list));
-}
-r.appendChild(todoListRoot);
-completenessUpdate();
+    notifyChange() { // called by children to notify the parent that a change occurred on the tree, affecting every node above it
+        if (this.parent) {
+            this.parent.notifyChange();
+        }
+    }
 
-var doomsday = {
-    code: "DoomsdaySuxxorz!!",
-    completionPoint: 0
-};
+    acceptChild(child) {
+        if (this.stub) {
+            var inp = $id($_("input", { type: "checkbox", class: "taskExpander" }));
+            this.element.$a(
+                inp,
+                $k($t($_("label", { class: "taskExpandButton", "for": inp.id })), function (){
+                    $(this.htmlFor).checked = !$(this.htmlFor).checked;
+                })
+            );
+        }
+        this.stub = false;
+        this.subtasks.push(child);
+        if (this.parent) {
+            this.parent.notifyChange();
+        }
+    }
 
-window.onkeyup = (evt) => {
-    if (evt.keyCode == 9) {
-        if (document.activeElement.onmouseover) {
-            document.activeElement.onmouseover();
-        }
-        else if (document.activeElement.tagName == "LABEL") {
-            document.activeElement.parentNode.onmouseover();
-        }
-    }
-    if (evt.key == "Enter") {
-        if (document.activeElement.tagName == "LABEL") {
-            var inp = $(document.activeElement.getAttribute("for"));
-            inp.checked = !inp.checked;
-        }
-        else if (document.activeElement.onclick && document.activeElement.tagName != "BUTTON") { // triggers onclick upon Enter for items that aren't used to that sort of thing
-            document.activeElement.onclick();
-            if (document.activeElement.tagName == "LI") {
-                $("content").focus();
-            }
-        }
-    }
-    if (evt.shiftKey && evt.altKey) {
-        if (evt.key == "D") {
-            if (document.activeElement.tagName == "LABEL" || document.activeElement.tagName == "LI") {
-                delThing();
-            }
-        }
-        if (evt.key == "N") {
-            if (document.activeElement.tagName == "LABEL") {
-                addThing();
-            }
-        }
-        if (evt.key == "M") {
-            if (document.activeElement.tagName == "LABEL") {
-                addDir();
-            }
-        }
-        if (evt.key == "R") {
-            percModeFrac = !percModeFrac;
-            completenessUpdate();
-        }
-    }
-    if (evt.key == "Shift") {
-        return;
-    }
-    if (evt.key == doomsday.code[doomsday.completionPoint]) {
-        doomsday.completionPoint++;
-        if (doomsday.completionPoint >= doomsday.code.length) {
-            localStorage.list = "[]";
-            Array.from(document.getElementsByTagName("input")).forEach(element => {
-                element.value = "";
-                element.checked = false;
-            });
-            location.reload();
-        }
-    }
-    else {
-        doomsday.completionPoint = 0;
-    }
-    if (evt.key == "Home") {
-        if (list.length > 0) {
-            if (Array.isArray(list[0])) {
-                list[0].el.querySelector("label").focus();
-            }
-            else {
-                list[0].el.focus();
-            }
-            list[0].el.onmouseover();
-        }
-    }
-};
-
-function settings() {
-    if ($("settingsMenu").style.display == "") {
-        $("settingsMenu").style.display = "none";
-    }
-    else {
-        $("settingsMenu").style.display = "";
-        $("paddings").focus();
+    attach(parent) { // attach to a task or task-like (Set)
+        // required instance methods for parent: acceptChild, getContainer, notifyChange
+        parent.acceptChild(this);
+        parent.getContainer().$a(this.element);
     }
 }
 
 
-function classism(thing) {
-    var choices = [];
-    for (var i = 0; i < thing.children.length; i++) {
-        choices.push(thing.children[i].value);
+class Set {
+    constructor(name) {
+        this.name = name;
+        this.tasks = [];
+        this.element = $_("div", {class: "set"});
     }
-    thing.onchange = () => {
-        choices.forEach(choice => {
-            document.body.classList.remove(thing.id + "-" + choice);
-        });
-        document.body.classList.add(thing.id + "-" + thing.value);
+
+    static unpack(data) { // load (parsed) Set JSON to create a Set
+
     }
-    thing.onchange();
+
+    pack() { // generate Set JSON
+
+    }
+
+    attach(element) { // attach this Set to the DOM
+        $a(element, this.element);
+    }
+
+    getContainer() {
+        return this.element;
+    }
+
+    acceptChild(child) {
+        this.tasks.push(child);
+    }
+
+    notifyChange() {
+
+    }
 }
 
-
-classism($("paddings"));
-classism($("colors"));
-classism($("fonts"));
-
-function pibald() {
-    fetch("https://swaous.asuscomm.com/piday", (data) => {
-        data.text().then(string => {
-            console.log(string);
-            for (var x = 0; x < string.length; x++) {
-                console.log(string[x]);
-            }
-        });
-    });
-}
-//pibald();
+var testSet = new Set("my shit");
+testSet.attach($("navInside"));
+var testTask = new Task("shit part 1", "shit content", 1);
+testTask.attach(testSet);
+var testTask2 = new Task("shit nested", "shit nested content", 1);
+testTask2.attach(testTask);
